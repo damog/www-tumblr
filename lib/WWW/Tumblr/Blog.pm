f04784ffa15172ee630abe0cb76e592297be874e
David Moreno <david@axiombox.com>
2013-07-21 00:30:48 +0200

    basic support for blog/post

--- lib/WWW/Tumblr/Blog.pm
+++ lib/WWW/Tumblr/Blog.pm
@@ -2,9 +2,12 @@ package WWW::Tumblr::Blog;
 use Moose;
 use Data::Dumper;
 
+use WWW::Tumblr::API;
 extends 'WWW::Tumblr';
 
 has 'base_hostname', is => 'rw', isa => 'Str', required => 1;
+
+tumblr_api_method post => [ 'POST', 'oauth' ];
 
 sub blog { ... }
 
