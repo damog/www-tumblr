d244f5cc04ace51cce8d42e8d4a3af7cf05f0b8e
David Moreno <david@axiombox.com>
2013-07-21 11:33:49 +0200

    Added more oauth methods - improved ResponseError

--- lib/WWW/Tumblr/ResponseError.pm
+++ lib/WWW/Tumblr/ResponseError.pm
@@ -7,6 +7,32 @@ use JSON 'decode_json';
 has 'response', is => 'rw', isa => 'HTTP::Response';
 
 sub code    { $_[0]->response->code }
-sub reasons  { decode_json( $_[0]->response->decoded_content)->{response}->{errors} }
+sub reasons  {
+    my $self = $_[0];
+    my $j = decode_json( $_[0]->response->decoded_content);
+    if ( ref $j && ref $j eq 'HASH' ) {
+        if ( ref $j->{response} && ref $j->{response} eq 'ARRAY' ) {
+            unless ( scalar @{ $j->{response} }) {
+                return [ $self->response->message ]
+            }
+            return $j->{response};
+        } elsif ( ref $j->{response} && ref $j->{response} eq 'HASH' &&
+            defined $j->{response}->{errors}  
+        ) {
+            if ( defined $j->{response}->{errors}->{state} ) {
+                return [ 
+                    $j->{response}->{errors}->{0},
+                    $j->{response}->{errors}->{state}
+                ];
+            } else {
+                return $j->{response}->{errors};
+            }
+        } else {
+            ...
+        }
+    } else {
+        ...
+    }
+}
 
 1;
