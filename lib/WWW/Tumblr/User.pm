9426c8a08761ed16f87bc428b4a6845c67f38c33
David Moreno <david@axiombox.com>
2013-08-13 23:26:08 +0200

    Adding remaining methods for User

--- lib/WWW/Tumblr/User.pm
+++ lib/WWW/Tumblr/User.pm
@@ -8,7 +8,8 @@ use WWW::Tumblr::API;
 
 extends 'WWW::Tumblr';
 
-tumblr_api_method $_, [ 'GET', 'oauth' ] for qw( info dashboard likes following );
+tumblr_api_method $_, [ 'GET',  'oauth' ] for qw( info dashboard likes following );
+tumblr_api_method $_, [ 'POST', 'oauth' ] for qw( follow unfollow like unlike );
 
 sub user { ... }
 
